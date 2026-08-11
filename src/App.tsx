74198ad542c851aafbcb490a81bfa2d421c23377
henriquemunizz <henriquedasilvamuniz03@gmail.com>
2026-08-11 20:02:40 -0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Button } from './Button'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <h1>hello world</h1>
+
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Button.tsx
@@ -0,0 +1,11 @@
+import "./button.css"
+
+
+export function Button() {
+
+
+    return (
+        <button>clique</button>
+    )
+
+}
